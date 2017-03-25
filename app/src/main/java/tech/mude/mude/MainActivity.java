6e94856ed8f63a8ec5ef4d2124ae3fe9c8060c0e
leoybkim <leoybkim@gmail.com>
2017-03-25 05:25:27 -0400

    Connect to estimote and print rssi

--- app/src/main/java/tech/mude/mude/MainActivity.java
+++ app/src/main/java/tech/mude/mude/MainActivity.java
@@ -1,16 +1,135 @@
 package tech.mude.mude;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothHeadset;
-import android.bluetooth.BluetoothProfile;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.EstimoteSDK;
+import com.estimote.sdk.Region;
+import com.estimote.sdk.SystemRequirementsChecker;
+import com.estimote.sdk.cloud.model.Color;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import tech.mude.mude.estimote.BeaconID;
+import tech.mude.mude.estimote.EstimoteCloudBeaconDetails;
+import tech.mude.mude.estimote.EstimoteCloudBeaconDetailsFactory;
+import tech.mude.mude.estimote.ProximityContentManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static BeaconManager mBeaconManager;
+    private ProximityContentManager mProximityContentManager;
+
+    private TextView mProximity;
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final Map<Color, Integer> BACKGROUND_COLORS = new HashMap<>();
+
+    static {
+        BACKGROUND_COLORS.put(Color.ICY_MARSHMALLOW, android.graphics.Color.rgb(109, 170, 199));
+        BACKGROUND_COLORS.put(Color.BLUEBERRY_PIE, android.graphics.Color.rgb(98, 84, 158));
+        BACKGROUND_COLORS.put(Color.MINT_COCKTAIL, android.graphics.Color.rgb(155, 186, 160));
+    }
+
+    private static final int BACKGROUND_COLOR_NEUTRAL = android.graphics.Color.rgb(160, 169, 172);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Setup token
+        EstimoteSDK.initialize(this, "beacons-wearhacks-gmail-co-fm7", "0d11e8a3491c51a383ab8c85b55929b7");
+
+        // Setup views
+        mProximity = (TextView) findViewById(R.id.proximity);
+
+        // Setup Beacon Manager
+        mBeaconManager = new BeaconManager(getApplicationContext());
+        mBeaconManager.setBackgroundScanPeriod(TimeUnit.SECONDS.toMillis(1), 0);
+        mBeaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                mBeaconManager.startMonitoring(new Region(
+                        "monitored region",
+                        UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"),
+                        null, null));
+            }
+        });
+
+        mBeaconManager.setMonitoringListener(new BeaconManager.MonitoringListener(){
+            @Override
+            public void onEnteredRegion(Region region, List<Beacon> list) {
+                Beacon nearestBeacon = list.get(0);
+                mProximity.setText(String.valueOf(nearestBeacon.getRssi()));
+            }
+
+            @Override
+            public void onExitedRegion(Region region) {
+
+            }
+        });
+
+        // Setup ProximityContentManager
+        mProximityContentManager = new ProximityContentManager(this,
+                Arrays.asList(
+                        new BeaconID("B9407F30-F5F8-466E-AFF9-25556B57FE6D", 33491, 34365)),
+                new EstimoteCloudBeaconDetailsFactory());
+
+        mProximityContentManager.setListener(new ProximityContentManager.Listener() {
+            @Override
+            public void onContentChanged(Object content) {
+                String text;
+                Integer backgroundColor;
+                if (content != null) {
+                    EstimoteCloudBeaconDetails beaconDetails = (EstimoteCloudBeaconDetails) content;
+                    text = "You're in " + beaconDetails.getBeaconName() + "'s range!";
+                    backgroundColor = BACKGROUND_COLORS.get(beaconDetails.getBeaconColor());
+
+                } else {
+                    text = "No beacons in range.";
+                    backgroundColor = null;
+                }
+                ((TextView) findViewById(R.id.textView)).setText(text);
+                findViewById(R.id.relativeLayout).setBackgroundColor(
+                        backgroundColor != null ? backgroundColor : BACKGROUND_COLOR_NEUTRAL);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (!SystemRequirementsChecker.checkWithDefaultDialogs(this)) {
+            Log.e(TAG, "Can't scan for beacons, some pre-conditions were not met");
+            Log.e(TAG, "Read more about what's required at: http://estimote.github.io/Android-SDK/JavaDocs/com/estimote/sdk/SystemRequirementsChecker.html");
+            Log.e(TAG, "If this is fixable, you should see a popup on the app's screen right now, asking to enable what's necessary");
+        } else {
+            Log.d(TAG, "Starting ProximityContentManager content updates");
+            mProximityContentManager.startContentUpdates();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "Stopping ProximityContentManager content updates");
+        mProximityContentManager.stopContentUpdates();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mProximityContentManager.destroy();
     }
 }
